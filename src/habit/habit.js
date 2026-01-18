effebd0c276b84dc2e068492813c320cfa715166
KHALID ABDULLAHI ISSE <khalidabdulahiesse@gmail.com>
2026-01-19 01:09:56 +0300

    complete habit project and link togather

--- src/habit/habit.js
+++ src/habit/habit.js
@@ -1,26 +1,27 @@
 const dayButtons = document.querySelectorAll(".day");
-const progressPercentage= document.getElementById("progress-text");
+const progressText = document.getElementById("progress-text");
+const progressFill = document.querySelector(".progress-fill");
 
+function updateProgress() {
+  let activeCount = 0;
 
+  dayButtons.forEach(btn => {
+    if (btn.classList.contains("active")) {
+      activeCount++;
+    }
+  });
 
+  const percentage = Math.round(
+    (activeCount / dayButtons.length) * 100
+  );
 
-
-
-
-
+  progressText.textContent = percentage + "%";
+  progressFill.style.width = percentage + "%";
+}
 
 dayButtons.forEach(button => {
   button.addEventListener("click", () => {
     button.classList.toggle("active");
+    updateProgress();
   });
-
-     let per = 0;
-
-    dayButtons.forEach(btn => {
-      if (btn.classList.contains("active")) {
-        per++;
-      }
-    });
-    
-
 });
